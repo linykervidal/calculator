69c1533171d67468cf35dc02f9227ee89c0c40bd
Línyker Vidal <linyker.theodoro@gmail.com>
2023-02-23 00:26:02 -0400

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,195 @@
+"use strict";
+const viewCalcs = document.getElementById('view-calcs');
+const ulNumbers = document.getElementById('numbers');
+const opSum = document.getElementById('sum');
+const opSub = document.getElementById('subtract');
+const opMult = document.getElementById('multiply');
+const opDiv = document.getElementById('divide');
+const opTotal = document.getElementById('total');
+const opReset = document.getElementById('reset');
+const decimal = document.getElementById('decimal');
+const pView = document.createElement('p');
+const resultHtml = document.createElement('p');
+pView.id = 'view-numbers';
+pView.innerText = '0';
+resultHtml.id = 'result';
+resultHtml.innerText = '= 0';
+viewCalcs === null || viewCalcs === void 0 ? void 0 : viewCalcs.appendChild(pView);
+viewCalcs === null || viewCalcs === void 0 ? void 0 : viewCalcs.appendChild(resultHtml);
+for (let i = 0; i < 10; i++) {
+    const liConstruct = document.createElement('li');
+    liConstruct.innerText = i.toString();
+    liConstruct.id = `num${i}`;
+    ulNumbers === null || ulNumbers === void 0 ? void 0 : ulNumbers.appendChild(liConstruct);
+    liConstruct.addEventListener('click', () => addNumber(i));
+    if (i === 0) {
+        liConstruct.style.order = '-1';
+    }
+}
+let numbers = [];
+let partialNumber;
+let partialResult;
+let resultCalc;
+let lastOp = [];
+let eventsActive = false;
+let usedOp = false;
+let isFloated = false;
+let isFinished = false;
+function resetStyle() {
+    pView.style.fontSize = '72px';
+    resultHtml.style.fontSize = '30px';
+}
+function addOpEvents() {
+    isFinished = false;
+    eventsActive = true;
+    opSum === null || opSum === void 0 ? void 0 : opSum.addEventListener('click', useSomeOp);
+    opSub === null || opSub === void 0 ? void 0 : opSub.addEventListener('click', useSomeOp);
+    opMult === null || opMult === void 0 ? void 0 : opMult.addEventListener('click', useSomeOp);
+    opDiv === null || opDiv === void 0 ? void 0 : opDiv.addEventListener('click', useSomeOp);
+}
+function addNumber(n) {
+    if (pView.innerText === '0' || isFinished) {
+        resetStyle();
+        pView.innerText = n.toString();
+        addOpEvents();
+    }
+    else if (usedOp) {
+        pView.innerText += n;
+        usedOp = false;
+    }
+    else {
+        pView.innerText += n;
+    }
+    partialNumber += n;
+    switch (lastOp[lastOp.length - 1]) {
+        case 'sum':
+            resultCalc = partialResult + parseFloat(partialNumber);
+            resultHtml.innerText = `= ${resultCalc}`;
+            break;
+        case 'sub':
+            resultCalc = partialResult - parseFloat(partialNumber);
+            resultHtml.innerText = `= ${resultCalc}`;
+            break;
+        case 'mult':
+            switch (lastOp[lastOp.length - 2]) {
+                case 'sum':
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber);
+                    break;
+                case 'sub':
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber);
+                    break;
+                default:
+                    resultCalc = partialResult * parseFloat(partialNumber);
+            }
+            resultHtml.innerText = `= ${resultCalc}`;
+            break;
+        case 'div':
+            switch (lastOp[lastOp.length - 2]) {
+                case 'sum':
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber);
+                    break;
+                case 'sub':
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber);
+                    break;
+                default:
+                    resultCalc = partialResult / parseFloat(partialNumber);
+            }
+            resultHtml.innerText = `= ${resultCalc}`;
+            break;
+        default:
+            resultHtml.innerText = `= ${pView.innerText}`;
+    }
+    resultHtml.style.opacity = '1';
+}
+function useSomeOp(e) {
+    numbers.push(parseFloat(partialNumber));
+    if (lastOp.length === 0) {
+        partialResult = numbers[0];
+    }
+    else {
+        partialResult = resultCalc;
+    }
+    if (isFloated) {
+        isFloated = false;
+    }
+    resultHtml.innerText = `= ${partialResult}`.replace('.', ',');
+    usedOp = true;
+    partialNumber = '';
+    switch (e.target) {
+        case opSum:
+            pView.innerHTML += ' +&nbsp;';
+            lastOp.push('sum');
+            break;
+        case opSub:
+            pView.innerHTML += ' -&nbsp;';
+            lastOp.push('sub');
+            break;
+        case opMult:
+            pView.innerHTML += ' x&nbsp;';
+            lastOp.push('mult');
+            break;
+        default:
+            pView.innerHTML += ' /&nbsp;';
+            lastOp.push('div');
+    }
+}
+function floatingNumber() {
+    if (!eventsActive) {
+        addOpEvents();
+    }
+    if (isFinished) {
+        resetStyle();
+        partialNumber = '0.';
+        pView.innerText = '0,';
+    }
+    else if (!isFloated) {
+        if (pView.innerText === '0') {
+            partialNumber = '0.';
+            pView.innerText += ',';
+            resultHtml.innerText = `= ${pView.innerText}`;
+            resultHtml.style.opacity = '1';
+        }
+        else if (partialNumber === '') {
+            partialNumber = '0.';
+            pView.innerText += ' 0,';
+        }
+        else {
+            partialNumber += '.';
+            pView.innerText += ',';
+        }
+    }
+    isFloated = true;
+}
+decimal === null || decimal === void 0 ? void 0 : decimal.addEventListener('click', floatingNumber);
+function clearDatas() {
+    numbers = [];
+    lastOp = [];
+    usedOp = false;
+    isFloated = false;
+    opSum === null || opSum === void 0 ? void 0 : opSum.removeEventListener('click', useSomeOp);
+    opSub === null || opSub === void 0 ? void 0 : opSub.removeEventListener('click', useSomeOp);
+    opMult === null || opMult === void 0 ? void 0 : opMult.removeEventListener('click', useSomeOp);
+    opDiv === null || opDiv === void 0 ? void 0 : opDiv.removeEventListener('click', useSomeOp);
+}
+function total() {
+    resultHtml.style.opacity = '1';
+    pView.style.transition = '0.5s ease-in-out';
+    resultHtml.style.transition = '0.5s ease-in-out';
+    pView.style.fontSize = '30px';
+    resultHtml.style.fontSize = '72px';
+    isFinished = true;
+    clearDatas();
+    setTimeout(() => {
+        pView.style.transition = 'none';
+        resultHtml.style.transition = 'none';
+    }, 600);
+}
+opTotal === null || opTotal === void 0 ? void 0 : opTotal.addEventListener('click', total);
+function resetCalc() {
+    pView.innerText = '0';
+    resultHtml.innerText = '= 0';
+    resetStyle();
+    resultHtml.style.opacity = '0';
+    clearDatas();
+}
+opReset === null || opReset === void 0 ? void 0 : opReset.addEventListener('click', resetCalc);
---
+++ src/index.ts
@@ -0,0 +1,231 @@
+type dom = HTMLElement | null //type alias
+
+//chamando elementos via DOM
+const viewCalcs: dom = document.getElementById('view-calcs') //tipando uma variável
+const ulNumbers: dom = document.getElementById('numbers')
+const opSum: dom = document.getElementById('sum')
+const opSub: dom = document.getElementById('subtract')
+const opMult: dom = document.getElementById('multiply')
+const opDiv: dom = document.getElementById('divide')
+const opTotal: dom = document.getElementById('total')
+const opReset: dom = document.getElementById('reset')
+const decimal: dom = document.getElementById('decimal')
+
+//criando elementos que serão geradas no HTML
+const pView: HTMLParagraphElement = document.createElement('p')
+const resultHtml: HTMLParagraphElement = document.createElement('p')
+
+//configurando elementos criados
+pView.id = 'view-numbers'
+pView.innerText = '0'
+resultHtml.id = 'result'
+resultHtml.innerText = '= 0'
+
+//atribuindo elementos filhos ao pai
+viewCalcs?.appendChild(pView)
+viewCalcs?.appendChild(resultHtml)
+
+//criando e configurando vários elementos com looping e atribuindo-as ao seu pai
+for (let i: number = 0; i < 10; i++) {
+    const liConstruct: HTMLLIElement = document.createElement('li')
+    liConstruct.innerText = i.toString()
+    liConstruct.id = `num${i}`
+    ulNumbers?.appendChild(liConstruct)
+    liConstruct.addEventListener('click', () => addNumber(i))
+    if (i === 0) {
+        liConstruct.style.order = '-1'
+    }
+}
+
+//criando variáveis que manipularão a lógica do projeto
+let numbers: number[] = []
+let partialNumber: string
+let partialResult: number
+let resultCalc: number
+let lastOp: string[] = []
+let eventsActive: boolean = false
+let usedOp: boolean = false
+let isFloated: boolean = false
+let isFinished: boolean = false
+
+
+//CRIANDO FUNÇÕES
+
+//resetando estilos
+function resetStyle(): void {
+    pView.style.fontSize = '72px'
+    resultHtml.style.fontSize = '30px'
+}
+
+//adicionando os eventos operacionais
+function addOpEvents(): void {
+    isFinished = false
+    eventsActive = true
+    opSum?.addEventListener('click', useSomeOp)
+    opSub?.addEventListener('click', useSomeOp)
+    opMult?.addEventListener('click', useSomeOp)
+    opDiv?.addEventListener('click', useSomeOp)
+}
+
+//adicionando números e realizando cálculos dinamicamente
+function addNumber(n: number): void {
+    if (pView.innerText === '0' || isFinished) {
+        resetStyle()
+        pView.innerText = n.toString()
+        addOpEvents()
+    } else if (usedOp) {
+        pView.innerText += n
+        usedOp = false
+    } else {
+        pView.innerText += n
+    }
+    partialNumber += n
+
+    switch (lastOp[lastOp.length - 1]) {
+        case 'sum':
+            resultCalc = partialResult + parseFloat(partialNumber)
+            resultHtml.innerText = `= ${resultCalc}`
+            break
+        case 'sub':
+            resultCalc = partialResult - parseFloat(partialNumber)
+            resultHtml.innerText = `= ${resultCalc}`
+            break
+        case 'mult':
+            switch (lastOp[lastOp.length - 2]) {
+                case 'sum':
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber)
+                    break
+                case 'sub':
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber)
+                    break
+                default:
+                    resultCalc = partialResult * parseFloat(partialNumber)
+            }
+            resultHtml.innerText = `= ${resultCalc}`
+            break
+        case 'div':
+            switch (lastOp[lastOp.length - 2]) {
+                case 'sum':
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber)
+                    break
+                case 'sub':
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber)
+                    break
+                default:
+                    resultCalc = partialResult / parseFloat(partialNumber)
+            }
+            resultHtml.innerText = `= ${resultCalc}`
+            break
+        default:
+            resultHtml.innerText = `= ${pView.innerText}`
+    }
+    resultHtml.style.opacity = '1'
+}
+
+//usando as operações
+function useSomeOp(e: Event): void {
+    numbers.push(parseFloat(partialNumber))
+
+    if (lastOp.length === 0) {
+        partialResult = numbers[0]
+    } else {
+        partialResult = resultCalc
+    }
+
+    if (isFloated) {
+        isFloated = false
+    }
+
+    resultHtml.innerText = `= ${partialResult}`.replace('.', ',')
+    usedOp = true
+    partialNumber = ''
+
+    //testando os elementos
+    switch (e.target) {
+        case opSum:
+            pView.innerHTML += ' +&nbsp;'
+            lastOp.push('sum')
+            break
+        case opSub:
+            pView.innerHTML += ' -&nbsp;'
+            lastOp.push('sub')
+            break
+        case opMult:
+            pView.innerHTML += ' x&nbsp;'
+            lastOp.push('mult')
+            break
+        default:
+            pView.innerHTML += ' /&nbsp;'
+            lastOp.push('div')
+    }
+}
+
+//PAREI AQUI, TÁ BUGADO AINDA
+function floatingNumber() {
+    if (!eventsActive) {
+        addOpEvents()
+    }
+
+    if (isFinished) {
+        resetStyle()
+        partialNumber = '0.'
+        pView.innerText = '0,'
+    } else if (!isFloated) {
+        if (pView.innerText === '0') {
+            partialNumber = '0.'
+            pView.innerText += ','
+            resultHtml.innerText = `= ${pView.innerText}`
+            resultHtml.style.opacity = '1'
+        } else if (partialNumber === '') {
+            partialNumber = '0.'
+            pView.innerText += ' 0,'
+        } else {
+            partialNumber += '.'
+            pView.innerText += ','
+        }
+    }
+
+    isFloated = true
+}
+
+decimal?.addEventListener('click', floatingNumber)
+
+//resetando algumas variáveis e eventos
+function clearDatas(): void {
+    numbers = []
+    lastOp = []
+    usedOp = false
+    isFloated = false
+    opSum?.removeEventListener('click', useSomeOp)
+    opSub?.removeEventListener('click', useSomeOp)
+    opMult?.removeEventListener('click', useSomeOp)
+    opDiv?.removeEventListener('click', useSomeOp)
+}
+
+//calculando o total
+function total(): void {
+    resultHtml.style.opacity = '1'
+    pView.style.transition = '0.5s ease-in-out'
+    resultHtml.style.transition = '0.5s ease-in-out'
+    pView.style.fontSize = '30px'
+    resultHtml.style.fontSize = '72px'
+    isFinished = true
+    clearDatas()
+    setTimeout(() => {
+        pView.style.transition = 'none'
+        resultHtml.style.transition = 'none'        
+    }, 600);
+}
+
+opTotal?.addEventListener('click', total) //chamando a função acima
+
+//resetando a calculadora
+function resetCalc(): void {
+    pView.innerText = '0'
+    resultHtml.innerText = '= 0'
+    resetStyle()
+    resultHtml.style.opacity = '0'
+    clearDatas()
+}
+
+opReset?.addEventListener('click', resetCalc) //chamando a função acima
