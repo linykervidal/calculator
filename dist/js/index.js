72e4c1d013962ac05da2c1054f0d542568faec10
Línyker Vidal <linyker.theodoro@gmail.com>
2023-02-26 23:15:22 -0400

    att percent function and some other stuffs

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,7 +7,9 @@ const opMult = document.getElementById('multiply');
 const opDiv = document.getElementById('divide');
 const opTotal = document.getElementById('total');
 const opReset = document.getElementById('reset');
+const btnErase = document.getElementById('erase');
 const decimal = document.getElementById('decimal');
+const btnPercent = document.getElementById('percent');
 const pView = document.createElement('p');
 const resultHtml = document.createElement('p');
 pView.id = 'view-numbers';
@@ -27,7 +29,9 @@ for (let i = 0; i < 10; i++) {
     }
 }
 let numbers = [];
-let partialNumber = '';
+let partialNumber = [''];
+let lastExpression;
+let lastSimbolOp = [];
 let partialResult;
 let resultCalc;
 let lastOp = [];
@@ -44,6 +48,7 @@ function addOpEvents() {
     opSub === null || opSub === void 0 ? void 0 : opSub.addEventListener('click', useSomeOp);
     opMult === null || opMult === void 0 ? void 0 : opMult.addEventListener('click', useSomeOp);
     opDiv === null || opDiv === void 0 ? void 0 : opDiv.addEventListener('click', useSomeOp);
+    btnPercent === null || btnPercent === void 0 ? void 0 : btnPercent.addEventListener('click', percent);
 }
 function addNumber(n) {
     if (pView.innerText === '0' || isFinished) {
@@ -54,44 +59,48 @@ function addNumber(n) {
     }
     else if (usedOp) {
         pView.innerText += n;
+        btnPercent === null || btnPercent === void 0 ? void 0 : btnPercent.addEventListener('click', percent);
         usedOp = false;
     }
     else {
         pView.innerText += n;
     }
-    partialNumber += n;
+    partialNumber[partialNumber.length - 1] += n;
+    attViewNumber();
+}
+function attViewNumber() {
     switch (lastOp[lastOp.length - 1]) {
         case 'sum':
-            resultCalc = partialResult + parseFloat(partialNumber);
+            resultCalc = partialResult + parseFloat(partialNumber[partialNumber.length - 1]);
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',');
             break;
         case 'sub':
-            resultCalc = partialResult - parseFloat(partialNumber);
+            resultCalc = partialResult - parseFloat(partialNumber[partialNumber.length - 1]);
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',');
             break;
         case 'mult':
             switch (lastOp[lastOp.length - 2]) {
                 case 'sum':
-                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber);
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1]);
                     break;
                 case 'sub':
-                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber);
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1]);
                     break;
                 default:
-                    resultCalc = partialResult * parseFloat(partialNumber);
+                    resultCalc = partialResult * parseFloat(partialNumber[partialNumber.length - 1]);
             }
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',');
             break;
         case 'div':
             switch (lastOp[lastOp.length - 2]) {
                 case 'sum':
-                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber);
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber[partialNumber.length - 1]);
                     break;
                 case 'sub':
-                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber);
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber[partialNumber.length - 1]);
                     break;
                 default:
-                    resultCalc = partialResult / parseFloat(partialNumber);
+                    resultCalc = partialResult / parseFloat(partialNumber[partialNumber.length - 1]);
             }
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',');
             break;
@@ -100,34 +109,66 @@ function addNumber(n) {
     }
 }
 function useSomeOp(e) {
-    numbers.push(parseFloat(partialNumber));
-    if (lastOp.length === 0) {
-        partialResult = numbers[0];
+    if (!usedOp) {
+        numbers.push(parseFloat(partialNumber[partialNumber.length - 1]));
+        if (lastOp.length === 0) {
+            partialResult = numbers[0];
+        }
+        else {
+            partialResult = resultCalc;
+        }
+        resultHtml.innerText = `= ${partialResult}`.replace('.', ',');
+        isFloated = false;
+        partialNumber.push('');
+        btnPercent === null || btnPercent === void 0 ? void 0 : btnPercent.removeEventListener('click', percent);
     }
-    else {
-        partialResult = resultCalc;
-    }
-    resultHtml.innerText = `= ${partialResult}`.replace('.', ',');
-    isFloated = false;
-    usedOp = true;
-    partialNumber = '';
     switch (e.target) {
         case opSum:
-            pView.innerHTML += ' +&nbsp;';
-            lastOp.push('sum');
+            if (!usedOp) {
+                lastExpression = pView.innerHTML;
+                lastOp.push('sum');
+                lastSimbolOp.push('+');
+            }
+            else {
+                lastOp[lastOp.length - 1] = 'sum';
+                lastSimbolOp[lastSimbolOp.length - 1] = '+';
+            }
             break;
         case opSub:
-            pView.innerHTML += ' -&nbsp;';
-            lastOp.push('sub');
+            if (!usedOp) {
+                lastExpression = pView.innerHTML;
+                lastOp.push('sub');
+                lastSimbolOp.push('-');
+            }
+            else {
+                lastOp[lastOp.length - 1] = 'sub';
+                lastSimbolOp[lastSimbolOp.length - 1] = '-';
+            }
             break;
         case opMult:
-            pView.innerHTML += ' x&nbsp;';
-            lastOp.push('mult');
+            if (!usedOp) {
+                lastExpression = pView.innerHTML;
+                lastOp.push('mult');
+                lastSimbolOp.push('x');
+            }
+            else {
+                lastOp[lastOp.length - 1] = 'mult';
+                lastSimbolOp[lastSimbolOp.length - 1] = 'x';
+            }
             break;
         default:
-            pView.innerHTML += ' /&nbsp;';
-            lastOp.push('div');
+            if (!usedOp) {
+                lastExpression = pView.innerHTML;
+                lastOp.push('div');
+                lastSimbolOp.push('/');
+            }
+            else {
+                lastOp[lastOp.length - 1] = 'div';
+                lastSimbolOp[lastSimbolOp.length - 1] = '/';
+            }
     }
+    pView.innerHTML = `${lastExpression} ${lastSimbolOp[lastSimbolOp.length - 1]}&nbsp;`;
+    usedOp = true;
 }
 function floatingNumber() {
     if (!isFloated) {
@@ -138,28 +179,81 @@ function floatingNumber() {
             addOpEvents();
             resultHtml.style.opacity = '1';
         }
-        else if (partialNumber === '' && usedOp) {
+        else if (partialNumber[partialNumber.length - 1] === '' && usedOp) {
             pView.innerText += '0,';
             usedOp = false;
         }
         else {
             pView.innerText += ',';
         }
-        partialNumber += '.';
+        partialNumber[partialNumber.length - 1] += '.';
         isFloated = true;
     }
 }
 decimal === null || decimal === void 0 ? void 0 : decimal.addEventListener('click', floatingNumber);
+function percent() {
+    if (numbers.length === 0) {
+        pView.innerText += '%';
+        resultHtml.innerText = `= ${parseFloat(partialNumber[partialNumber.length - 1]) / 100}`.replace('.', ',');
+        total();
+    }
+    else {
+        if (lastOp[lastOp.length - 1] === 'mult' || lastOp[lastOp.length - 1] === 'div') {
+            if (lastOp[lastOp.length - 2] === 'sum' || lastOp[lastOp.length - 2] === 'sub') {
+                pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;');
+                partialNumber[partialNumber.length - 1] = (numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString();
+                pView.innerText += partialNumber[partialNumber.length - 1];
+            }
+            else {
+                pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;');
+                partialNumber[partialNumber.length - 1] = (partialResult * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString();
+                pView.innerText += partialNumber;
+            }
+        }
+        else {
+            pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;');
+            partialNumber[partialNumber.length - 1] = (partialResult * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString();
+            pView.innerText += partialNumber;
+        }
+        attViewNumber();
+    }
+}
+function erase() {
+    if (numbers.length === 0) {
+        resetCalc();
+    }
+    else {
+        if (partialNumber[partialNumber.length - 1] === '0' || partialNumber[partialNumber.length - 1] === '') {
+            numbers.pop();
+            partialNumber.pop();
+            lastOp.pop();
+            usedOp = false;
+            pView.innerHTML = pView.innerHTML.replace(` ${lastSimbolOp[lastSimbolOp.length - 1]}&nbsp;`, '');
+            lastSimbolOp.pop();
+        }
+        else {
+            if (partialNumber[partialNumber.length - 1].startsWith('0')) {
+                partialNumber[partialNumber.length - 1] = partialNumber[partialNumber.length - 1].replace('0', '');
+            }
+            pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;');
+            partialNumber[partialNumber.length - 1] = '0';
+            attViewNumber();
+        }
+    }
+}
+btnErase === null || btnErase === void 0 ? void 0 : btnErase.addEventListener('click', erase);
 function clearDatas() {
     numbers = [];
     lastOp = [];
-    partialNumber = '';
+    lastSimbolOp = [];
+    partialNumber = [''];
     isFloated = false;
     usedOp = false;
     opSum === null || opSum === void 0 ? void 0 : opSum.removeEventListener('click', useSomeOp);
     opSub === null || opSub === void 0 ? void 0 : opSub.removeEventListener('click', useSomeOp);
     opMult === null || opMult === void 0 ? void 0 : opMult.removeEventListener('click', useSomeOp);
     opDiv === null || opDiv === void 0 ? void 0 : opDiv.removeEventListener('click', useSomeOp);
+    btnPercent === null || btnPercent === void 0 ? void 0 : btnPercent.removeEventListener('click', percent);
 }
 function total() {
     resultHtml.style.opacity = '1';
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ const opMult: dom = document.getElementById('multiply')
 const opDiv: dom = document.getElementById('divide')
 const opTotal: dom = document.getElementById('total')
 const opReset: dom = document.getElementById('reset')
+const btnErase: dom = document.getElementById('erase')
 const decimal: dom = document.getElementById('decimal')
+const btnPercent: dom = document.getElementById('percent')
 
 //criando elementos que serão geradas no HTML
 const pView: HTMLParagraphElement = document.createElement('p')
@@ -39,7 +41,9 @@ for (let i: number = 0; i < 10; i++) {
 
 //criando variáveis que manipularão a lógica do projeto
 let numbers: number[] = []
-let partialNumber: string = ''
+let partialNumber: string[] = ['']
+let lastExpression: string
+let lastSimbolOp: string[] = []
 let partialResult: number
 let resultCalc: number
 let lastOp: string[] = []
@@ -63,9 +67,10 @@ function addOpEvents(): void {
     opSub?.addEventListener('click', useSomeOp)
     opMult?.addEventListener('click', useSomeOp)
     opDiv?.addEventListener('click', useSomeOp)
+    btnPercent?.addEventListener('click', percent)
 }
 
-//adicionando números e realizando cálculos dinamicamente
+//adicionando números
 function addNumber(n: number): void {
     if (pView.innerText === '0' || isFinished) {
         resetStyle()
@@ -74,44 +79,49 @@ function addNumber(n: number): void {
         resultHtml.style.opacity = '1'
     } else if (usedOp) {
         pView.innerText += n
+        btnPercent?.addEventListener('click', percent)
         usedOp = false
     } else {
         pView.innerText += n
     }
-    partialNumber += n
+    partialNumber[partialNumber.length - 1] += n
+    attViewNumber()
+}
 
+//realizando cálculos dinamicamente
+function attViewNumber():void {
     switch (lastOp[lastOp.length - 1]) {
         case 'sum':
-            resultCalc = partialResult + parseFloat(partialNumber)
+            resultCalc = partialResult + parseFloat(partialNumber[partialNumber.length - 1])
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',')
             break
         case 'sub':
-            resultCalc = partialResult - parseFloat(partialNumber)
+            resultCalc = partialResult - parseFloat(partialNumber[partialNumber.length - 1])
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',')
             break
         case 'mult':
             switch (lastOp[lastOp.length - 2]) {
                 case 'sum':
-                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber)
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1])
                     break
                 case 'sub':
-                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber)
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1])
                     break
                 default:
-                    resultCalc = partialResult * parseFloat(partialNumber)
+                    resultCalc = partialResult * parseFloat(partialNumber[partialNumber.length - 1])
             }
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',')
             break
         case 'div':
             switch (lastOp[lastOp.length - 2]) {
                 case 'sum':
-                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber)
+                    resultCalc = partialResult - numbers[numbers.length - 1] + numbers[numbers.length - 1] / parseFloat(partialNumber[partialNumber.length - 1])
                     break
                 case 'sub':
-                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber)
+                    resultCalc = partialResult + numbers[numbers.length - 1] - numbers[numbers.length - 1] / parseFloat(partialNumber[partialNumber.length - 1])
                     break
                 default:
-                    resultCalc = partialResult / parseFloat(partialNumber)
+                    resultCalc = partialResult / parseFloat(partialNumber[partialNumber.length - 1])
             }
             resultHtml.innerText = `= ${resultCalc}`.replace('.', ',')
             break
@@ -122,37 +132,65 @@ function addNumber(n: number): void {
 
 //usando as operações
 function useSomeOp(e: Event): void {
-    numbers.push(parseFloat(partialNumber))
-
-    if (lastOp.length === 0) {
-        partialResult = numbers[0]
-    } else {
-        partialResult = resultCalc
+    if (!usedOp) {        
+        numbers.push(parseFloat(partialNumber[partialNumber.length - 1]))
+        
+        if (lastOp.length === 0) {
+            partialResult = numbers[0]
+        } else {
+            partialResult = resultCalc
+        }
+        
+        resultHtml.innerText = `= ${partialResult}`.replace('.', ',')
+        isFloated = false
+        partialNumber.push('')
+        btnPercent?.removeEventListener('click', percent)
     }
-
-    resultHtml.innerText = `= ${partialResult}`.replace('.', ',')
-    isFloated = false
-    usedOp = true
-    partialNumber = ''
 
     //testando os elementos
     switch (e.target) {
         case opSum:
-            pView.innerHTML += ' +&nbsp;'
-            lastOp.push('sum')
+            if (!usedOp) {
+                lastExpression = pView.innerHTML
+                lastOp.push('sum')
+                lastSimbolOp.push('+')
+            } else {
+                lastOp[lastOp.length - 1] = 'sum'
+                lastSimbolOp[lastSimbolOp.length - 1] = '+'
+            }
             break
         case opSub:
-            pView.innerHTML += ' -&nbsp;'
-            lastOp.push('sub')
+            if (!usedOp) {
+                lastExpression = pView.innerHTML
+                lastOp.push('sub')
+                lastSimbolOp.push('-')
+            } else {
+                lastOp[lastOp.length - 1] = 'sub'
+                lastSimbolOp[lastSimbolOp.length - 1] = '-'
+            }
             break
         case opMult:
-            pView.innerHTML += ' x&nbsp;'
-            lastOp.push('mult')
+            if (!usedOp) {
+                lastExpression = pView.innerHTML
+                lastOp.push('mult')
+                lastSimbolOp.push('x')
+            } else {
+                lastOp[lastOp.length - 1] = 'mult'
+                lastSimbolOp[lastSimbolOp.length - 1] = 'x'
+            }
             break
         default:
-            pView.innerHTML += ' /&nbsp;'
-            lastOp.push('div')
+            if (!usedOp) {
+                lastExpression = pView.innerHTML
+                lastOp.push('div')
+                lastSimbolOp.push('/')
+            } else {
+                lastOp[lastOp.length - 1] = 'div'
+                lastSimbolOp[lastSimbolOp.length - 1] = '/'
+            }
     }
+    pView.innerHTML = `${lastExpression} ${lastSimbolOp[lastSimbolOp.length - 1]}&nbsp;`
+    usedOp = true
 }
 
 //tornando o numero decimal
@@ -164,30 +202,83 @@ function floatingNumber(): void {
             resultHtml.innerText = '= 0,'
             addOpEvents()
             resultHtml.style.opacity = '1'
-        } else if (partialNumber === '' && usedOp) {
+        } else if (partialNumber[partialNumber.length - 1] === '' && usedOp) {
             pView.innerText += '0,'
             usedOp = false
         } else {
             pView.innerText += ','
         }
-        partialNumber += '.'
+        partialNumber[partialNumber.length - 1] += '.'
         isFloated = true
     }
 }
 
-decimal?.addEventListener('click', floatingNumber)
+decimal?.addEventListener('click', floatingNumber) //chamando a função acima
+
+//usando porcentagem sobre o último número
+function percent(): void {
+    if (numbers.length === 0) {
+        pView.innerText += '%'
+        resultHtml.innerText = `= ${parseFloat(partialNumber[partialNumber.length - 1]) / 100}`.replace('.', ',')
+        total()
+    } else {
+        if (lastOp[lastOp.length - 1] === 'mult' || lastOp[lastOp.length - 1] === 'div') {
+            if (lastOp[lastOp.length - 2] === 'sum' || lastOp[lastOp.length - 2] === 'sub') {
+                pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;')
+                partialNumber[partialNumber.length - 1] = (numbers[numbers.length - 1] * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString()
+                pView.innerText += partialNumber[partialNumber.length - 1]
+            } else {
+                pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;')
+                partialNumber[partialNumber.length - 1] = (partialResult * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString()
+                pView.innerText += partialNumber
+            }
+        } else {
+            pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;')
+            partialNumber[partialNumber.length - 1] = (partialResult * parseFloat(partialNumber[partialNumber.length - 1]) / 100).toString()
+            pView.innerText += partialNumber
+        }
+        attViewNumber()
+    }
+}
+
+//PAREI AQUI, FUNÇÃO AINDA ESTÁ BUGADA, TESTAR TORNAR A VARIAVEL LAST EXPRESSION COMO ARRAY E INCLUIR NA SOLUÇÃO
+function erase(): void {
+    if (numbers.length === 0) {
+        resetCalc()
+    } else {
+        if (partialNumber[partialNumber.length - 1] === '0' || partialNumber[partialNumber.length - 1] === '') {
+            numbers.pop()
+            partialNumber.pop()
+            lastOp.pop()
+            usedOp = false
+            pView.innerHTML = pView.innerHTML.replace(` ${lastSimbolOp[lastSimbolOp.length - 1]}&nbsp;`, '')
+            lastSimbolOp.pop()
+        } else {
+            if (partialNumber[partialNumber.length - 1].startsWith('0')) {
+                partialNumber[partialNumber.length - 1] = partialNumber[partialNumber.length - 1].replace('0', '')
+            }
+            pView.innerHTML = pView.innerHTML.replace(`&nbsp;${partialNumber[partialNumber.length - 1]}`, '&nbsp;')
+            partialNumber[partialNumber.length - 1] = '0'
+            attViewNumber()
+        }
+    }
+}
+
+btnErase?.addEventListener('click', erase)
 
 //resetando algumas variáveis e eventos
 function clearDatas(): void {
     numbers = []
     lastOp = []
-    partialNumber = ''
+    lastSimbolOp = []
+    partialNumber = ['']
     isFloated = false
     usedOp = false
     opSum?.removeEventListener('click', useSomeOp)
     opSub?.removeEventListener('click', useSomeOp)
     opMult?.removeEventListener('click', useSomeOp)
     opDiv?.removeEventListener('click', useSomeOp)
+    btnPercent?.removeEventListener('click', percent)
 }
 
 //calculando o total
